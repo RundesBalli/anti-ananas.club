fec9d9dfe3364c2de47dbd498e33f533b747d1fe
RundesBalli <GitHub@RundesBalli.com>
2022-05-21 13:01:53 +0200

    Roboto Font hinzugefügt

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
       <a href="/">↻ reload</a>
     </div>
     <div id="footer">
-      <span>Ein Satireprojekt von: <a href="https://RundesBalli.com/" target="_blank" rel="noopener">RundesBalli.com</a><a href="https://RundesBalli.com/imprint" target="_blank" rel="noopener">Imprint</a></span><span>Sources: <a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a><a href="https://www.dafont.com/de/fair-prosper.font" target="_blank" rel="noopener">Font</a></span><span>ketzerische Gegenbewegung: <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>
+      <span>Ein Satireprojekt von: <a href="https://RundesBalli.com/" target="_blank" rel="noopener">RundesBalli.com</a><a href="https://RundesBalli.com/imprint" target="_blank" rel="noopener">Imprint</a></span><span><a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a></span><span>Fonts: <a href="https://fonts.google.com/specimen/Roboto" target="_blank" rel="noopener">Roboto</a><a href="https://www.dafont.com/de/fair-prosper.font" target="_blank" rel="noopener">Fair Prosper</a></span><span>ketzerische Gegenbewegung: <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>
     </div>
     <script type="text/javascript" src="/src/reload.js"></script>
   </body>
