05de307aba866685442927d5433a156b0106602f
RundesBalli <GitHub@RundesBalli.com>
2022-07-10 19:33:43 +0200

    Verzeichnisstruktur geändert und Hintergrundfarbe angepasst

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <meta name="robots" content="index, follow"/>
     <meta name="keywords" content="Ananas, Pineapple, Anti-Ananas, Anti"/>
     <meta name="revisit-after" content="3 days"/>
-    <link href="/src/style.css" rel="stylesheet">
-    <link rel="shortcut icon" href="/src/favicon.png" type="image/png">
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/png">
     <meta name="theme-color" content="#000000">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:title" content="Anti-Ananas.Club">
@@ -16,10 +16,9 @@
     <meta property="og:url" content="https://anti-ananas.club/">
     <meta property="og:description" content="Die Seite für Ananas-Hasser! Zeig jedem, dass du etwas gegen Ananas, Toast- und Pizza-Hawaii und andere Nahrungsmittelverunstaltungen hast!">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="https://anti-ananas.club/src/ananas.png">
+    <meta property="og:image" content="https://anti-ananas.club/assets/images/ananas.png">
   </head>
   <body>
-    <div id="bgimg"></div>
     <div id="wrapper">
       <div id="preamble">Ananas ist</div>
       <div id="description"><?php $desc = array("ekelhaft", "widerlich", "Abfall", "kotzfördernd", "Versagerfraß", "räudig", "Ranzfraß", "eine Unsitte", "abscheulich", "Nahrung der Unterschicht", "abstoßend", "unerträglich", "Nahrung dritter Klasse", "die Ausgeburt der Hölle", "scheußlich", "entsetzlich", "untragbar", "schändlich", "Kotze zum Essen", "doof", "wertloser Müll", "Schund"); echo $desc[array_rand($desc)]; ?></div>
@@ -28,6 +27,6 @@
     <div id="footer">
       <span>Ein Satireprojekt von: <a href="https://RundesBalli.com/" target="_blank" rel="noopener">RundesBalli.com</a><a href="https://RundesBalli.com/imprint" target="_blank" rel="noopener">Imprint</a></span><span><a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a></span><span>Fonts: <a href="https://fonts.google.com/specimen/Roboto" target="_blank" rel="noopener">Roboto</a><a href="https://www.dafont.com/de/fair-prosper.font" target="_blank" rel="noopener">Fair Prosper</a></span><span>ketzerische Gegenbewegung: <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>
     </div>
-    <script type="text/javascript" src="/src/reload.js"></script>
+    <script type="text/javascript" src="/assets/js/reload.js"></script>
   </body>
 </html>
