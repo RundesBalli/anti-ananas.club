03f86874ae0b6857cd0b58be76ce432057862d94
RundesBalli <GitHub@RundesBalli.com>
2022-10-13 12:18:26 +0200

    Added <ENTER> reload from @NullDev

--- assets/js/reload.js
+++ assets/js/reload.js
@@ -3,6 +3,10 @@ let doReload = true;
 window.onblur = () => (doReload = false);
 window.onfocus = () => (doReload = true);
 
+window.onkeydown = e => {
+  if (e.keyCode === 13) window.location.reload();
+};
+
 setInterval(() => {
   if (doReload) location.reload();
 }, 5000);
