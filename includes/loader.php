1a102f71e797eb3781135a3d8041bf689d0b276b
RundesBalli <GitHub@RundesBalli.com>
2023-10-31 23:57:56 +0100

    Added phrases

--- includes/loader.php
+++ includes/loader.php
@@ -8,6 +8,7 @@
 /**
  * Content generation and router
  */
+require_once(__DIR__.DIRECTORY_SEPARATOR.'phrases.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'readTemplate.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'routing'.DIRECTORY_SEPARATOR.'routes.php');
 require_once(__DIR__.DIRECTORY_SEPARATOR.'generation'.DIRECTORY_SEPARATOR.'footer.php');
---
+++ includes/phrases.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * includes/phrases.php
+ * 
+ * All phrases with preambles
+ */
+
+$phrases = [
+  [
+    'Ananas ist',
+    'ekelhaft',
+  ],
+  [
+    'Ananas ist',
+    'widerlich',
+  ],
+  [
+    'Ananas ist',
+    'Abfall',
+  ],
+  [
+    'Ananas ist',
+    'kotzfördernd',
+  ],
+  [
+    'Ananas ist',
+    'Versagerfraß',
+  ],
+  [
+    'Ananas ist',
+    'räudig',
+  ],
+  [
+    'Ananas ist',
+    'Ranzfraß',
+  ],
+  [
+    'Ananas ist',
+    'eine Unsitte',
+  ],
+  [
+    'Ananas ist',
+    'abscheulich',
+  ],
+  [
+    'Ananas ist',
+    'Nahrung der Unterschicht',
+  ],
+  [
+    'Ananas ist',
+    'abstoßend',
+  ],
+  [
+    'Ananas ist',
+    'unerträglich',
+  ],
+  [
+    'Ananas ist',
+    'Nahrung dritter Klasse',
+  ],
+  [
+    'Ananas ist',
+    'die Ausgeburt der Hölle',
+  ],
+  [
+    'Ananas ist',
+    'scheußlich',
+  ],
+  [
+    'Ananas ist',
+    'Höllenfraß',
+  ],
+  [
+    'Ananas ist',
+    'entsetzlich',
+  ],
+  [
+    'Ananas ist',
+    'untragbar',
+  ],
+  [
+    'Ananas ist',
+    'schändlich',
+  ],
+  [
+    'Ananas ist',
+    'Kotze zum Essen',
+  ],
+  [
+    'Ananas ist',
+    'doof',
+  ],
+  [
+    'Ananas ist',
+    'wertloser Müll',
+  ],
+  [
+    'Ananas ist',
+    'Schund',
+  ],
+  [
+    'Ananas riecht wie',
+    'Erbrochenes',
+  ],
+  [
+    'Toast-Hawaii',
+    'verstößt gegen Völkerrecht',
+  ],
+];
+?>
