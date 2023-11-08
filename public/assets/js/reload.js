bb8015096cd739261929133c78a2af8815ca0076
NullDev (Shadow) <chris@nulldev.org>
2023-11-08 16:58:01 +0100

    fixes and finished bar

--- public/assets/js/reload.js
+++ public/assets/js/reload.js
@@ -2,55 +2,70 @@
 
 let doReload = true;
 let progressInterval;
-let reloadInterval;
+let currentProgressWidth = 1;
 
+/**
+ * Fills the progress bar.
+ *
+ * @return {void}
+ */
 const fillBar = function(){
   const bar = document.getElementById("bar");
   if (!bar) return;
 
   clearInterval(progressInterval);
 
-  let width = 1;
+  bar.style.width = currentProgressWidth + "%";
   progressInterval = setInterval(() => {
-    if (width >= 100) {
+    if (currentProgressWidth >= 100){
       clearInterval(progressInterval);
-    } else {
-      width++;
-      bar.style.width = width + "%";
+      currentProgressWidth = 1;
+      bar.style.width = currentProgressWidth + "%";
+      if (doReload) loadPhrase();
+      return;
     }
+    
+    currentProgressWidth++;
+    bar.style.width = currentProgressWidth + "%";
   }, 50);
 };
 
-const loadPhrase = function(){
+/**
+ * Loads the phrase from the server.
+ * Needs to be hoisted.
+ */
+function loadPhrase(){
   fetch("/phrase", { mode: "cors" })
       .then(res => res.json())
       .then(data => {
           const preamble = document.getElementById("preamble");
           if (preamble) preamble.textContent = data.preamble;
 
-          const msg = document.getElementById("message")
+          const msg = document.getElementById("message");
           if (msg) msg.textContent = data.message;
 
           fillBar();
       });
 };
 
-window.onblur = () => (doReload = false);
-window.onfocus = () => (doReload = true);
+window.onblur = function(){
+  doReload = false;
+  clearInterval(progressInterval);
+};
+
+window.onfocus = function(){
+  doReload = true;
+  fillBar();
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const reloadBtn = document.getElementById("reload");
   if (!reloadBtn) return;
 
   reloadBtn.addEventListener("click", () => {
+    currentProgressWidth = 1;
     loadPhrase();
   });
 });
 
-(() => {
-  reloadInterval = setInterval(() => {
-    if (doReload) loadPhrase();
-  }, 5000);
-
-  loadPhrase();
-})();
+(() => loadPhrase())();
