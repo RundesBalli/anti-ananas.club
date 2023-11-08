39af15abc7af356a76b5e76a4839e1cbade4fbef
NullDev (Shadow) <chris@nulldev.org>
2023-11-08 17:17:35 +0100

    refactored everything

--- public/assets/js/reload.js
+++ public/assets/js/reload.js
@@ -1,71 +1,103 @@
 "use strict";
 
-let doReload = true;
-let progressInterval;
-let currentProgressWidth = 1;
-
 /**
- * Fills the progress bar.
+ * Loads a new phrase from the server and displays it on the page.
+ * Handle the progress bar, interval and reload button.
  *
- * @return {void}
+ * @class PhraseLoader
  */
-const fillBar = function(){
-  const bar = document.getElementById("bar");
-  if (!bar) return;
+class PhraseLoader {
+  /**
+   * Creates an instance of PhraseLoader.
+   *
+   * @memberof PhraseLoader
+   */
+  constructor(options = {}){
+    const { steps } = options;
 
-  clearInterval(progressInterval);
+    this.progressInterval = null;
+    this.currentProgressWidth = 1;
+    this.steps = steps ?? 50;
 
-  bar.style.width = currentProgressWidth + "%";
-  progressInterval = setInterval(() => {
-    if (currentProgressWidth >= 100){
-      clearInterval(progressInterval);
-      currentProgressWidth = 1;
-      bar.style.width = currentProgressWidth + "%";
-      if (doReload) loadPhrase();
-      return;
-    }
-    
-    currentProgressWidth++;
-    bar.style.width = currentProgressWidth + "%";
-  }, 50);
-};
+    window.onblur = () => this.#clearProgressInterval();
+    window.onfocus = () => this.fillBar();
 
-/**
- * Loads the phrase from the server.
- * Needs to be hoisted.
- */
-function loadPhrase(){
-  fetch("/phrase", { mode: "cors" })
+    document.addEventListener("DOMContentLoaded", this.initialize.bind(this));
+  }
+
+  /**
+   * Clears the progress interval.
+   *
+   * @memberof PhraseLoader
+   */
+  #clearProgressInterval(){
+    clearInterval(this.progressInterval ?? undefined);
+  }
+
+  /**
+   * Fills the progress bar.
+   *
+   * @param {boolean} [reset=false] - Whether to reset the progress bar.
+   * @return {void} 
+   * @memberof PhraseLoader
+   */
+  fillBar(reset = false){
+    const bar = document.getElementById("bar");
+    if (!bar) return;
+
+    this.#clearProgressInterval();
+    if (reset) this.currentProgressWidth = 1;
+
+    bar.style.width = `${this.currentProgressWidth}%`;
+    this.progressInterval = setInterval(() => {
+      if (this.currentProgressWidth >= 100) {
+        this.#clearProgressInterval();
+        this.currentProgressWidth = 1;
+        bar.style.width = `${this.currentProgressWidth}%`;
+        this.loadPhrase();
+        return;
+      }
+
+      this.currentProgressWidth++;
+      bar.style.width = `${this.currentProgressWidth}%`;
+    }, this.steps);
+  }
+
+  /**
+   * Loads a new phrase from the server and displays it on the page.
+   *
+   * @param {boolean} [reset=false] - Whether to reset the progress bar.
+   * @memberof PhraseLoader
+   */
+  loadPhrase(reset = false){
+    fetch("/phrase", { mode: "cors" })
       .then(res => res.json())
       .then(data => {
-          const preamble = document.getElementById("preamble");
-          if (preamble) preamble.textContent = data.preamble;
+        const preamble = document.getElementById("preamble");
+        if (preamble) preamble.textContent = data.preamble;
 
-          const msg = document.getElementById("message");
-          if (msg) msg.textContent = data.message;
+        const msg = document.getElementById("message");
+        if (msg) msg.textContent = data.message;
 
-          fillBar();
+        this.fillBar(reset);
       });
-};
+  }
 
-window.onblur = function(){
-  doReload = false;
-  clearInterval(progressInterval);
-};
+  /**
+   * Initializes the phrase loader.
+   *
+   * @public
+   * @memberof PhraseLoader
+   */
+  initialize(){
+    const reloadBtn = document.getElementById("reload");
+    if (reloadBtn) reloadBtn.addEventListener("click", () => this.loadPhrase(true));
 
-window.onfocus = function(){
-  doReload = true;
-  fillBar();
-};
+    this.loadPhrase();
+  }
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-  const reloadBtn = document.getElementById("reload");
-  if (!reloadBtn) return;
-
-  reloadBtn.addEventListener("click", () => {
-    currentProgressWidth = 1;
-    loadPhrase();
-  });
-});
-
-(() => loadPhrase())();
+(() => {
+  const phaseLoader = new PhraseLoader({ steps: 50 });
+  document.addEventListener("DOMContentLoaded", () => phaseLoader.initialize());
+})();
