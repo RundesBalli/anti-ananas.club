5f48b62a40e10a01ae2d90a4e13eeb8e753c16e0
RundesBalli <GitHub@RundesBalli.com>
2024-12-17 20:54:58 +0100

    Added locale changer to footer

--- includes/generation/footer.php
+++ includes/generation/footer.php
@@ -9,4 +9,18 @@ $footer =
 '<span><a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a></span>'.
 '<span>Fonts: <a href="https://fonts.google.com/specimen/Roboto" target="_blank" rel="noopener">Roboto</a><a href="https://www.dafont.com/de/fair-prosper.font" target="_blank" rel="noopener">Fair Prosper</a></span>'.
 '<span>ketzerische Gegenbewegung: <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>';
+/**
+ * Locale changer
+ */
+$localeLinks = '';
+foreach($availableLocales as $key => $val) {
+  if($locale == $key) {
+    /**
+     * Skip current used locale
+     */
+    continue;
+  }
+  $localeLinks.= '<a href="/changeLocale?locale='.$key.'" class="linkNoUnderline"><img class="flag" title="'.(!empty($lang['footer']['locales'][$val['locale']]) ? $lang['footer']['locales'][$val['locale']] : NULL).'" src="/assets/images/flags/'.$val['imageFile'].'.svg"></a>';
+}
+$footer.= '<span>'.$localeLinks.'</span>';
 ?>
