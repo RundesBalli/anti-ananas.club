e52b58355e896ca6993160f195b45c0170cb13d3
RundesBalli <GitHub@RundesBalli.com>
2024-12-17 21:46:41 +0100

    Added english locale

---
+++ includes/locales/en.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * includes/locales/en.php
+ * 
+ * English locale
+ */
+$lang = [
+  /**
+   * Footer elements
+   */
+  'footer' => [
+    'satire' => 'A satire project by',
+    'imprint' => 'Imprint',
+    'info' => 'Info',
+    'antiAnti' => 'Heretical countermovement', # Title for the anti-anti-ananas.club
+
+    /**
+     * Title description for the flags
+     */
+    'locales' => [
+      'de' => 'German',
+    ]
+  ],
+
+  /**
+   * OG tags
+   */
+  'og' => [
+    'description' => 'The site for pineapple haters! Show everyone that you have something against pineapple, toast and pizza Hawaii and other food disfigurements!',
+  ],
+
+  /**
+   * Phrases
+   */
+  'phrases' => [
+    [
+      'The word "Pineapple" is',
+      'wrong! It\'s Ananas',
+    ],
+    [
+      'Pineapple is',
+      'disgusting',
+    ],
+    [
+      'Pineapple is',
+      'nasty',
+    ],
+    [
+      'Pineapple is',
+      'nauseating',
+    ],
+    [
+      'Pineapple is',
+      'Waste',
+    ],
+    [
+      'Pineapple is',
+      'vomit inducing',
+    ],
+    [
+      'Pineapple is',
+      'food for losers',
+    ],
+    [
+      'Pineapple is',
+      'fruit for losers',
+    ],
+    [
+      'Pineapple is',
+      'rancid food',
+    ],
+    [
+      'Pineapple is',
+      'a bad taste',
+    ],
+    [
+      'Pineapple is',
+      'hideous',
+    ],
+    [
+      'Pineapple is',
+      'food for the lower class',
+    ],
+    [
+      'Pineapple is',
+      'intolerable',
+    ],
+    [
+      'Pineapple is',
+      'third class food',
+    ],
+    [
+      'Pineapple is',
+      'the spawn of hell',
+    ],
+    [
+      'Pineapple is',
+      'dreadful',
+    ],
+    [
+      'Pineapple is',
+      'awful',
+    ],
+    [
+      'Pineapple is',
+      'food from hell',
+    ],
+    [
+      'Pineapple is',
+      'horrifying',
+    ],
+    [
+      'Pineapple is',
+      'unacceptable',
+    ],
+    [
+      'Pineapple is',
+      'infamous',
+    ],
+    [
+      'Pineapple is',
+      'vomit for dinner',
+    ],
+    [
+      'Pineapple is',
+      'dumb',
+    ],
+    [
+      'Pineapple is',
+      'stupid',
+    ],
+    [
+      'Pineapple is',
+      'worthless garbage',
+    ],
+    [
+      'Pineapple is',
+      'trash',
+    ],
+    [
+      'Ananas riecht wie',
+      'vomit',
+    ],
+    [
+      'Toast-Hawaii',
+      'violates international law',
+    ],
+  ],
+];
+?>
