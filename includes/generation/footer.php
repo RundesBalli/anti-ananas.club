de5e17c70c21595d27b78aee93e3e824d77d1226
RundesBalli <GitHub@RundesBalli.com>
2024-12-17 20:55:05 +0100

    Updated links in footer

--- includes/generation/footer.php
+++ includes/generation/footer.php
@@ -5,10 +5,10 @@
  * Footer generation
  */
 $footer =
-'<span>Ein Satireprojekt von: <a href="https://RundesBalli.com/" target="_blank" rel="noopener">RundesBalli.com</a><a href="https://RundesBalli.com/imprint" target="_blank" rel="noopener">Imprint</a></span>'.
-'<span><a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a></span>'.
-'<span>Fonts: <a href="https://fonts.google.com/specimen/Roboto" target="_blank" rel="noopener">Roboto</a><a href="https://www.dafont.com/de/fair-prosper.font" target="_blank" rel="noopener">Fair Prosper</a></span>'.
-'<span>ketzerische Gegenbewegung: <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>';
+'<span>'.$lang['footer']['satire'].': <a href="https://RundesBalli.com/" target="_blank" rel="noopener">RundesBalli.com</a><a href="https://RundesBalli.com/imprint" target="_blank" rel="noopener">'.$lang['footer']['imprint'].'</a></span>'.
+'<span><a href="https://github.com/RundesBalli/anti-ananas.club" target="_blank" rel="noopener">GitHub</a><a href="/info">'.$lang['footer']['info'].'</a></span>'.
+'<span>'.$lang['footer']['antiAnti'].': <a href="https://anti-anti-ananas.club" target="_blank" rel="noopener">anti-anti-ananas.club</a></span>';
+
 /**
  * Locale changer
  */
