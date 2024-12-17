3cd4ef9bd8e5825642eaded37edb8fe7a5bb778f
RundesBalli <GitHub@RundesBalli.com>
2024-12-17 20:54:37 +0100

    Fill meta tags

--- includes/generation/generateOutput.php
+++ includes/generation/generateOutput.php
@@ -7,11 +7,17 @@
 $randomKey = array_rand($phrases);
 $output = preg_replace(
   [
+    '/{LANG}/im',
+    '/{OGDESCRIPTION}/im',
+    '/{OGLOCALE}/im',
     '/{CONTENT}/im',
     '/{FOOTER}/im',
     '/{RELOADJS}/im',
   ],
   [
+    $locale,
+    $lang['og']['description'],
+    $availableLocales[$locale]['ogLocale'],
     $content,
     $footer,
     ($reloadjs ? '<script type="text/javascript" src="/assets/js/reload.js"></script>' : NULL),
