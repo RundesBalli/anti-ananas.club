0c1857515c3a286aa8303913672943f9afed1bf0
RundesBalli <GitHub@RundesBalli.com>
2024-12-17 21:02:55 +0100

    Added German locale

---
+++ includes/locales/de.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * includes/locales/de.php
+ * 
+ * German locale
+ */
+$lang = [
+  /**
+   * Footer elements
+   */
+  'footer' => [
+    'satire' => 'Ein Satireprojekt von',
+    'imprint' => 'Impressum',
+    'info' => 'Info',
+    'antiAnti' => 'ketzerische Gegenbewegung', # Title for the anti-anti-ananas.club
+
+    /**
+     * Title description for the flags
+     */
+    'locales' => [
+      'en' => 'Englisch',
+    ]
+  ],
+
+  /**
+   * OG tags
+   */
+  'og' => [
+    'description' => 'Die Seite für Ananas-Hasser! Zeig jedem, dass du etwas gegen Ananas, Toast- und Pizza-Hawaii und andere Nahrungsmittelverunstaltungen hast!',
+  ],
+
+  /**
+   * Phrases
+   */
+  'phrases' => [
+    [
+      'Ananas ist',
+      'ekelhaft',
+    ],
+    [
+      'Ananas ist',
+      'widerlich',
+    ],
+    [
+      'Ananas ist',
+      'Abfall',
+    ],
+    [
+      'Ananas ist',
+      'kotzfördernd',
+    ],
+    [
+      'Ananas ist',
+      'Versagerfraß',
+    ],
+    [
+      'Ananas ist',
+      'Versagerobst',
+    ],
+    [
+      'Ananas ist',
+      'räudig',
+    ],
+    [
+      'Ananas ist',
+      'Ranzfraß',
+    ],
+    [
+      'Ananas ist',
+      'eine Unsitte',
+    ],
+    [
+      'Ananas ist',
+      'abscheulich',
+    ],
+    [
+      'Ananas ist',
+      'Nahrung der Unterschicht',
+    ],
+    [
+      'Ananas ist',
+      'abstoßend',
+    ],
+    [
+      'Ananas ist',
+      'unerträglich',
+    ],
+    [
+      'Ananas ist',
+      'Nahrung dritter Klasse',
+    ],
+    [
+      'Ananas ist',
+      'die Ausgeburt der Hölle',
+    ],
+    [
+      'Ananas ist',
+      'scheußlich',
+    ],
+    [
+      'Ananas ist',
+      'Höllenfraß',
+    ],
+    [
+      'Ananas ist',
+      'entsetzlich',
+    ],
+    [
+      'Ananas ist',
+      'untragbar',
+    ],
+    [
+      'Ananas ist',
+      'schändlich',
+    ],
+    [
+      'Ananas ist',
+      'Kotze zum Essen',
+    ],
+    [
+      'Ananas ist',
+      'doof',
+    ],
+    [
+      'Ananas ist',
+      'wertloser Müll',
+    ],
+    [
+      'Ananas ist',
+      'Schund',
+    ],
+    [
+      'Ananas riecht wie',
+      'Erbrochenes',
+    ],
+    [
+      'Toast-Hawaii',
+      'verstößt gegen Völkerrecht',
+    ],
+  ],
+];
+?>
